8e4f19462a8e3d50582fc630bc3ef332d11852ab
Allison Demirok-Winters <allisonwinters.studio@gmail.com>
2023-12-22 10:44:41 -0800

    Check sprint1-dev branch

--- allison-winters-brainflix/src/App.js
+++ allison-winters-brainflix/src/App.js
@@ -1,8 +1,13 @@
 import Header from "./components/Header.js";
+import Player from "./components/Player.js";
 
 const App = () => {
   return (
+    <>
     <Header />
+    <Player />
+
+    </>
   )
 }
 
---
+++ allison-winters-brainflix/src/components/Player.js
@@ -0,0 +1,5 @@
+import React from "react";
+import Logo from "../assets/images/logos/BrainFlix-logo.svg";
+import Upload from "../assets/images/icons/upload.svg";
+import User from "../assets/images/Mohan-muruge.jpg"
+import "../styles/Main.scss";
