d178b13183644cb79da353c276c13547ab08f41f
Allison Demirok-Winters <allisonwinters.studio@gmail.com>
2023-12-26 16:54:16 -0800

    Begin building structure of comments section and stylings

--- allison-winters-brainflix/src/components/Player.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import Logo from "../assets/images/logos/BrainFlix-logo.svg";
-import Upload from "../assets/images/icons/upload.svg";
-import User from "../assets/images/Mohan-muruge.jpg"
-import "../styles/Main.scss";
