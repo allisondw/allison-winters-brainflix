03da703002c0d3ed77860fb8b961a3c6318591c7
Allison Demirok-Winters <allisonwinters.studio@gmail.com>
2023-12-29 15:18:59 -0800

    Pull mistake dev branch into local dev branch, hopefully

--- allison-winters-brainflix/src/App.js
+++ allison-winters-brainflix/src/App.js
@@ -4,18 +4,16 @@ import videoDetailsData from "../src/data/video-details.json"
 import videosList from "../src/data/videos.json";
 import Header from "./components/Header/Header.js";
 import Hero from "./components/Hero/Hero.js";
+import About from "./components/About/About.js";
 import Comments from "./components/Comments/Comments.js";
 import NextVideo from "./components/NextVideo/NextVideo.js";
-import { useEffect } from 'react';
 
 const App = () => {
 
   const [videoData, setVideoData] = useState(videoDetailsData[0]);
 
   const handleVideoChange = ((newVideoID) => {
-    console.log('Selected Video ID:', newVideoID);
     const selectedVideo = videoDetailsData.find((video) => video.id === newVideoID);
-    console.log('Selected Video:', selectedVideo); 
     if (selectedVideo) {
       setVideoData(selectedVideo);
     } else {
@@ -23,31 +21,25 @@ const App = () => {
     }
   })
 
-  useEffect(() => {
-    console.log('Updated videoData:', videoData);
-  }, [videoData]);
-
   const filteredVideos = videosList.filter((video) => video.id !== videoData.id);
   return (
     <body>
         <Header />
       <main>
         <Hero videoData = {videoData} />
-        <section className="main__left-column">
-       {/* <About videoData = {videoData} /> */}
-        <Comments videoData = {videoData} />
+        <section className="main-below">
+          <article className="main__left-column">
+            <About videoData = {videoData} />
+            <Comments videoData = {videoData} />
+          </article>
+          <article className="main__sidebar">
+            <NextVideo videos = {filteredVideos}
+            onVideoSelect = {handleVideoChange}
+            currentVideoID = {videoData.id}/>
+          </article>
         </section>
-        <section className="main__sidebar">
-          <NextVideo videos = {filteredVideos}
-          onVideoSelect = {handleVideoChange}
-          currentVideoID = {videoData.id}/>
-        </section>
-
-
       </main>
     </body>
   )
 }
-
-
 export default App;
